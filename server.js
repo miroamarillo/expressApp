55d80419654389e5b563367f75d0a772be81658d
Julian Pineda <studio@miroamarillo.com>
2014-10-21 19:55:28 -0400

    More comments about middleware

--- server.js
+++ server.js
@@ -19,7 +19,16 @@ function log(req, res, next){
 var names = [];
 
 app
+	//This is what is called a third party middleware
 	.use(bodyParser.urlencoded())
+	//Custom middleware can also be created.  Notice that when created
+	//and then applied with use, it will be used by every method. That
+	//will be the diference between use and all
+	//This is an example of a custom made middleware
+	.use(function(req, res, next){
+		console.log("This will log in every request");
+		next();
+	})
 	//This will mach ALL the requests
 	.all('/', function(req, res, next){
 		//Notice that this logs to the termina (server) not to the browser!
@@ -27,6 +36,7 @@ app
 		//This function will let the process cascade to the next methods
 		next();
 	})
+	//The functions bellow can also be called route functions
 	//Notice How I pass the log function to the get request
 	.get('/', log, function(req, res){
 		res.render('index.jade', {
@@ -40,6 +50,16 @@ app
 		//redirect to the home page
 		res.redirect('/');
 	})
+
+	//app.put
+	//app.delete
+	//Implement on your own this two methods
+
+	//built-in middleware
+	//This built-in middleware is used mainly for static files like
+	//images, css, etc.
+	use(express.static('./public'))
+
 	.listen(3080, function(){
 		console.log("Listening on port 3080");
 })
