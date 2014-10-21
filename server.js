5f5082ac54dcb4459eed96e8022f5e722fa8bb76
Julian Pineda <studio@miroamarillo.com>
2014-10-21 19:23:12 -0400

    Huege amount of changes.  Read all the comments!

--- server.js
+++ server.js
@@ -1,11 +1,44 @@
 var express 	= require('express'),
+	//This is needed to parse the body of the request
+	bodyParser 	= require('body-parser'),
 	app 		= express();
 
+//CRUD - The Verbs!!
+//POST - Create
+//GET - Read
+//PUT - Update
+//DELETE - Delete
+
+//let's create a function to log the updates to the array
+
+function log(req, res, next){
+	console.log(names);
+	next();
+}
+
+var names = [];
+
 app
-	.get('/', function(req, res){
+	.use(bodyParser.urlencoded())
+	//This will mach ALL the requests
+	.all('/', function(req, res, next){
+		//Notice that this logs to the termina (server) not to the browser!
+		console.log('Message coming from ALL');
+		//This function will let the process cascade to the next methods
+		next();
+	})
+	//Notice How I pass the log function to the get request
+	.get('/', log, function(req, res){
 		res.render('index.jade', {
-			title: "Hello Express & Jade and more stuff"
+			title: "Hello Express & Jade and more stuff",
+			names: names
 		});
+	})
+	.post('/', function(req, res){
+		//push the new value to the array
+		names.push(req.body.name);
+		//redirect to the home page
+		res.redirect('/');
 	})
 	.listen(3080, function(){
 		console.log("Listening on port 3080");
