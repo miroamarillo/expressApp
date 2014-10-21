0315ed9ffa3c742d17cc28f1ad9979a1fcc82a26
Julian Pineda <studio@miroamarillo.com>
2014-10-21 18:40:49 -0400

    Using jade as a template engine

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ var express 	= require('express'),
 
 app
 	.get('/', function(req, res){
-		res.send('Hello Express');
+		res.render('index.jade', {
+			title: "Hello Express & Jade and more stuff"
+		});
 	})
 	.listen(3080, function(){
 		console.log("Listening on port 3080");
