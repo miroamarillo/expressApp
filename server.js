dfe3286b1ff27061b21b9517f37f0bed6273e8ae
Julian Pineda <studio@miroamarillo.com>
2014-10-22 21:41:16 -0400

    several notes about the param object

--- server.js
+++ server.js
@@ -22,7 +22,8 @@ app
 	//This is what is called a third party middleware
 	.use(bodyParser.urlencoded())
 	//Custom middleware can also be created.  Notice that when created
-	//and then applied with use, it will be used by every method. That
+	//and then applied with use, it will be used by every object because
+	//it is not linked to an specific route like the all object. That
 	//will be the diference between use and all
 	//This is an example of a custom made middleware
 	.use(function(req, res, next){
@@ -33,7 +34,7 @@ app
 	.all('/', function(req, res, next){
 		//Notice that this logs to the termina (server) not to the browser!
 		console.log('Message coming from ALL');
-		//This function will let the process cascade to the next methods
+		//This function will let the process cascade to the next objects
 		next();
 	})
 	//The functions bellow can also be called route functions
@@ -50,15 +51,51 @@ app
 		//redirect to the home page
 		res.redirect('/');
 	})
+	//Params object
+	//The params 'name' that I'm using here matches the parameter used
+	//in the get object below.  It can be anythng!!
+	//Because the process goes from top to bottom, this object has to above
+	//the object that is going to use the parameter.
+	//It gives the posibility to pass another parameter that can be named
+	//however I want.  In this case, I will call it value and the
+	//parameter 'value' matches the token 'name' that I want to work with,
+	.param('name', function(req, res, next, value){
+		req.value = value[0].toUpperCase() + value.substring(1);
+		next();
+		//Param can be useful to grab info fro login for example
+		Users.findOne({username: name}, function(err, user){
+			req.user = user;
+			next();
+		})
+	})
+	//Nootice the diference between this one and the one below.
+	.get('/name/:name', function(req, res){
+		res.send('Your name is ' + req.value);
+	})
+	//Explanation of route parameters and how to use them
+	//The parameter comes after the colons
+	//to test check: http://localhost:3080/name/Jack
+	//Don't use this one combined with the param objec.  It won't work.
+	// .get('/name/:name', function(req, res){
+	// 	res.send('Your name is ' + req.params.name);
+	// })
 
+
+	//Implement on your own this two objects
 	//app.put
 	//app.delete
-	//Implement on your own this two methods
 
+
+	//Using routes
+	.get('/route', function(req, res){
+		res.send('This is a route');
+	})
 	//built-in middleware
 	//This built-in middleware is used mainly for static files like
 	//images, css, etc.
-	use(express.static('./public'))
+	//example of how to use express.static built-in middleware
+	//to test check: http://localhost:3080/file.txt
+	.use(express.static('./public'))
 
 	.listen(3080, function(){
 		console.log("Listening on port 3080");
