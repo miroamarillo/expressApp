674358665b7890f5eaa7c7f09285c50e980a806b
Julian Pineda <studio@miroamarillo.com>
2014-10-25 12:49:30 -0400

    cleaning server for Router Object

--- server.js
+++ server.js
@@ -4,33 +4,12 @@ var express 	= require('express'),
 	app 		= express();
 
 
-
-var names = [];
-
-app.use(bodyParser.urlencoded());
-	//Introducing the route method.
-app
-	.route('/')
-		//the route method can receive the methods below
-		// .all()
-		// .get()
-		// .post()
-		// .put()
-		// .delete()
-		.all(function(req, res, next){
-			console.log('Message coming from ALL');
-			next();
-		})
-		.get(function(req, res, next){
-			res.render('index.jade', {
-				title: "Hello Express & Jade and more stuff",
-				names: names
-			});
-		})
-		.post(function(req, res){
-			names.push(req.body.name);
-			res.redirect('/');
-		});
+//Introducing the Router Object
+//The Router object can get all of the previous methods
+// - use
+// - param
+// - verbs (get, post, put, delete) and the all method
+// - route
 
 app.listen(3080, function(){
 		console.log("Listening on port 3080");
