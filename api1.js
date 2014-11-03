f9ec3b28c4e1f241dcbbd427882c65cfe4410ea4
Julian Pineda <studio@miroamarillo.com>
2014-11-02 21:02:01 -0500

    API example with the Router Object

---
+++ api1.js
@@ -0,0 +1,7 @@
+var APIv1 = require('express').Router();
+
+APIv1.get('/names', function(req, res){
+	res.send('first from API 1');
+});
+
+module.exports = APIv1;
---
+++ api2.js
@@ -0,0 +1,7 @@
+var APIv2 = require('express').Router();
+
+APIv2.get('/names', function(req, res){
+	res.send('first from API 2');
+});
+
+module.exports = APIv2;
