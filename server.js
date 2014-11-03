859563bc81c2697b0742701069eef8082e06541d
Julian Pineda <studio@miroamarillo.com>
2014-11-03 13:47:53 -0500

    examples of req object

--- server.js
+++ server.js
@@ -4,58 +4,28 @@ var express 	= require('express'),
 	app 		= express();
 
 
-//Introducing the Router Object
-//The Router object can get all of the previous methods
-// - use
-// - param
-// - verbs (get, post, put, delete) and the all method
-// - route
-
-//Creating the Router Object
-// var router = express.Router({
-// 	//Default optins.  Can be removed.
-// 	caseSensitive: false,
-// 	strict: true
-// });
-
-// router
-// 	.use(function(req, res, next){
-// 		console.log('router specific middleware');
-// 		next();
-// 	})
-// 	.get('/', function(req, res){
-// 		res.send('router home route');
-// 	})
-// 	.route('/test')
-// 		.get(function(req, res){
-// 			res.send('router test route');
-// 		});
-
-// app.use('/', router);
-
-//API versions example
-
-// var APIv1 = express.Router(),
-// 	APIv2 = express.Router();
-
-// APIv1.get('/names', function(req, res){
-// 	res.send('first from API 1');
-// });
-
-// APIv2.get('/names', function(req, res){
-// 	res.send('first from API 2');
-// });
-
-//All this config goes into the api files.
-
-var APIv1 	= require('./api1'),
-	APIv2 	= require('./api2');
-
-//Now tell express what to use
-
-app
-	.use('/api/v1', APIv1)
-	.use('/api/v2', APIv2);
+app.get('/:nameOfDog', function(req, res){
+	//Request params
+	//params is an object from the request
+	//the parameters "nameOfDog" will be available
+	req.params.nameOfDog;
+	//Where ATTR is what goes after the parameter
+	//ie: :nameOfDog?name=Fryda
+	req.query.ATTR;
+	// body ATTR
+	req.body.ATTR;
+	//param method
+	req.param('ATTR'); //it will search in this order: params, body, query
+	//route method or object
+	req.route;
+	//request to original Url
+	req.originalUrl;
+	// If cookies middleware is used
+	req.cookies.ATTR;
+	req.get(); // any header name
+	//Objects specific for special headers
+	req.accepts('text/html'); //application/json or text/plain
+})
 
 app.listen(3080, function(){
 		console.log("Listening on port 3080");
