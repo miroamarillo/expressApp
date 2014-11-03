1362062abc29cf987c50c30750ea58d9c9fb1179
Julian Pineda <studio@miroamarillo.com>
2014-11-02 20:13:52 -0500

    Simple example of the Router Object

--- server.js
+++ server.js
@@ -11,6 +11,28 @@ var express 	= require('express'),
 // - verbs (get, post, put, delete) and the all method
 // - route
 
+//Creating the Router Object
+var router = express.Router({
+	//Default optins.  Can be removed.
+	caseSensitive: false,
+	strict: true
+});
+
+router
+	.use(function(req, res, next){
+		console.log('router specific middleware');
+		next();
+	})
+	.get('/', function(req, res){
+		res.send('router home route');
+	})
+	.route('/test')
+		.get(function(req, res){
+			res.send('router test route');
+		});
+
+app.use('/', router);
+
 app.listen(3080, function(){
 		console.log("Listening on port 3080");
 });
